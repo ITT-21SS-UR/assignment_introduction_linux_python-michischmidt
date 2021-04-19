39658d925b02bed982a72836420207bea7631f97
Michael Schmidt <schmidt.m.dev@googlemail.com>
2021-04-20 01:32:15 +0200

    finished exerice

--- count.sh
+++ count.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
 
-# downloading text file if not present in folder TODO change to wget
+# downloading text file if not present in folder
 if [ ! -e README ]
   then
-    curl -O ftp://sunsite.informatik.rwth-aachen.de/pub/mirror/ibiblio/gnome/README
+    wget ftp://sunsite.informatik.rwth-aachen.de/pub/mirror/ibiblio/gnome/README
 fi
 
 # convert file content to lowercase
 tr '[:upper:]' '[:lower:]' < README |
 
 # remove unnecessary characters and numbers
-sed 's|[:/.,0-9]||g'
+sed 's|[:/.,0-9]||g' |
+
+# remove line breaks
+tr -d "\n" |
+
+# remove multiple spaces
+tr -s " " |
+
+# split every word into a new line
+sed 's/ /\n/g;' |
+
+# sort alphabetically and remove duplicate words
+sort | uniq -c |
+
+# reverse sort the count of words and print the 10 most common words
+sort -r | head
