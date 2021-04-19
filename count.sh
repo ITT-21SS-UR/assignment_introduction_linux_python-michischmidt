7ea74d325d17773cd2eacf98e0f64a569307e43d
Michael Schmidt <schmidt.m.dev@googlemail.com>
2021-04-20 01:13:17 +0200

    added download, lowercase and character removal

--- count.sh
+++ count.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
 
+# downloading text file if not present in folder TODO change to wget
+if [ ! -e README ]
+  then
+    curl -O ftp://sunsite.informatik.rwth-aachen.de/pub/mirror/ibiblio/gnome/README
+fi
+
+# convert file content to lowercase
+tr '[:upper:]' '[:lower:]' < README |
+
+# remove unnecessary characters and numbers
+sed 's|[:/.,0-9]||g'
