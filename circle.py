f99abd0d8bdd1ae971eed3e5a54e08699d2fef76
Michael Schmidt <schmidt.m.dev@googlemail.com>
2021-04-21 02:14:10 +0200

    finished exercise

--- circle.py
+++ circle.py
@@ -5,19 +5,34 @@ import math
 
 def draw_circle(radius):
     turtle.color('green')
-    turtle.speed("fast")
-    counter_y_crossed = False
+    turtle.speed("fastest")
+    counter_y_crossed = 0
     x_sign = 1.0
-    while not counter_y_crossed:
+    while counter_y_crossed <= 1:
         turtle.forward(2 * math.pi * radius / 360.0)
         turtle.right(1.0)
         new_x = math.copysign(1, turtle.xcor())   
         if (new_x != x_sign):
-            counter_y_crossed = True
+            counter_y_crossed += 1
         x_sign = new_x
     turtle.done()
-    turtle.circle()
+
+
+def draw_good_circle(radius):
+    turtle.color('green')
+    turtle.dot(radius + 1)
+    turtle.color('white')
+    turtle.dot(radius - 1)
+    turtle.done()
 
 
 if __name__ == "__main__":
-    draw_circle(int(sys.argv[1]))
+    print("Choose drawing bad circle 1 or good circle 2:")
+    input = int(sys.stdin.readline())
+
+    if (input == 1): 
+        draw_circle(int(sys.argv[1]))
+    elif (input == 2):
+        draw_good_circle(int(sys.argv[1]))
+    else: 
+        print("Either choose 1 or 2 a option!")
